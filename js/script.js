7ce88204eeb1be83f6fcf90136696c39fd5c5fa7
duncan <lvivduncan@gmail.com>
2021-05-11 14:46:59 +0300

    home page

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+
+// слайдер на головній
+$('#carousel').owlCarousel({
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    items: 1,
+    margin: 30,
+    stagePadding: 30,
+    smartSpeed: 450,
+    dots: true,
+    autoplay: true,
+    loop: true
+});
+    

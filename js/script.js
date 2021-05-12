995f0bbdf394339d29d0799fab2c329e931cb611
duncan <lvivduncan@gmail.com>
2021-05-12 17:09:16 +0300

    item page

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ $('#carousel').owlCarousel({
     loop: true
 });
     
+// tabs
+(function($) {
+    $(function() {
+        $('ul.tabs-caption').on('click', 'li:not(.active)', function() {
+            $(this).addClass('active')
+                    .siblings()
+                    .removeClass('active')
+                    .closest('div.tabs')
+                    .find('div.tabs-content')
+                    .removeClass('active')
+                    .eq($(this).index())
+                    .addClass('active');
+        });
+    });
+})(jQuery);
+

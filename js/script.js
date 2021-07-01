7aed61c8dfc7a5428503b57d8cf334dbd08bee57
lvivduncan <lvivduncan@gmail.com>
2021-07-01 11:52:18 +0300

    single-article

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// window.onload = function(){
-    
-    // if(window.innerWidth < 1100){
-
-    //     levusTooltip();
-    // }
-    
-// }
-
 /**
  * стікед навбар
  */
@@ -517,44 +508,6 @@ const lightboxDescription = GLightbox({
             li = item.querySelectorAll('li');
         }
 
-        // item.innerHTML += '<div class="levus-nav"><span class="left"></span><span class="right"></span></div>';
-/* 
-        item.addEventListener('click', e => {
-            const ul = item.querySelector('ul');
-            if (e.target.className == 'left') {
-
-                // move last element
-                const last = ul.lastElementChild;
-                ul.prepend(last);
-
-                // destroy transition
-                ul.style.transition = 'none';
-                ul.classList.add('to-right');
-                setTimeout(() => {
-                    ul.classList.remove('to-right');
-                    ul.style.transition = '.5s';
-                }, 50);
-            }
-        });
-
-        item.addEventListener('click', e => {
-            const ul = item.querySelector('ul');
-            if (e.target.className == 'right') {
-
-                // move first element
-                const first = ul.firstElementChild;
-                ul.append(first);
-
-                // destroy transition
-                ul.style.transition = 'none';
-                ul.classList.add('to-left');
-                setTimeout(() => {
-                    ul.classList.remove('to-left');
-                    ul.style.transition = '.5s';
-                }, 50);
-            }
-        });
- */
         setInterval(() => {
             const ul = item.querySelector('ul');
 
